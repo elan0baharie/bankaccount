5bc80b866674ff551f73e177e3c0fe0c54c14132
Elan Baharie <0elan1@gmail.com>
2017-04-05 20:53:52 -0800

    add account updating functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,36 +9,51 @@ var Account = function (name, newDeposit, withdrawal, currentAmount) {
 }
 
 Account.prototype.makeDeposit = function() {
-  var newBalance= this.newDeposit + this.balance;
-  return newBalance;
+  var depTest = document.getElementById("newDeposit");
+  var withTest = document.getElementById("withdrawal");
 
+  if (depTest = "") {
+    depTest.replace("", 0);
+  } else if (withTest = "") {
+    withTest.replace("", 0);
+  } else {
+    var newBalance = this.newDeposit + this.balance - this.withdrawal;
+    this.balance = newBalance;
+    document.getElementById("newDeposit").value = "0";
+    document.getElementById("withdrawal").value = "0";
+    return newBalance;
+  }
+  console.log(this.balance);
 
 }
+
+
 
 //U.I.
 $(document).ready(function() {
 
   var newAccount = new Account();
 
+  document.getElementById("newDeposit").value = "0";
+  document.getElementById("withdrawal").value = "0";
+
   $("form#formone").submit(function(event) {
     event.preventDefault();
 
     newAccount.name = $("input#name").val();
     newAccount.balance = parseInt($("#bankAmount").val());
-    newAccount.newDeposit = parseInt($("#newDeposit").val());
-    newAccount.withdrawal = parseInt($("#withdrawal").val());
+    $(".orig-amnt").hide(800);
 
-    console.log(newAccount.name);
-    console.log(newAccount.balance);
-    // console.log(newAccount.name);
-    // console.log(newAccount.name);
     $("ul#test").append("<li>" + newAccount.balance +"</li>");
 
   });
 
   $("form#formtwo").submit(function(event) {
     event.preventDefault();
+    newAccount.newDeposit = parseInt($("#newDeposit").val());
+    newAccount.withdrawal = parseInt($("#withdrawal").val());
     var test = newAccount.makeDeposit();
     $("ul#test").append("<li>" + test +"</li>");
+    // newAccount.clearBal();
   });
 });
